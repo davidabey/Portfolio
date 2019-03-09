175c7f25bc6e8375ade9d63ac0d7ce5c2ea97acb
DAVID ABEYSINGHE <davidabey1999@gmail.com>
2019-03-09 14:49:54 +0100

    third commit

---
+++ js/f1.js
@@ -0,0 +1,14 @@
+
+    $('.js-colorlib-nav-toggle').on('click', function(event){
+        event.preventDefault();
+        var $this = $(this);
+
+        if ($('body').hasClass('offcanvas')) {
+            $this.removeClass('active');
+            $('body').removeClass('offcanvas');	
+        } else {
+            $this.addClass('active');
+            $('body').addClass('offcanvas');	
+        }
+    });
+
